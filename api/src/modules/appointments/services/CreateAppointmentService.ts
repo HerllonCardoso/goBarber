4d0ba8375ca45ca74d63e7fb1d23ba65a9bc3d3e
Herllon Cardoso <cardoso.herllon@gmail.com>
2021-01-04 16:51:56 -0300

    feat: appointments cache

--- api/src/modules/appointments/services/CreateAppointmentService.ts
+++ api/src/modules/appointments/services/CreateAppointmentService.ts
@@ -3,6 +3,8 @@ import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
@@ -21,6 +23,9 @@ class CreateAppoitmentService {
 
     @inject('NotificationsRepository')
     private notificationsRepository: INotificationsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({
@@ -62,6 +67,13 @@ class CreateAppoitmentService {
       recipient_id: provider_id,
       content: `Novo agendamento para dia ${dateFormatted}`,
     });
+
+    await this.cacheProvider.invalidate(
+      `provider-appointments:${provider_id}:${format(
+        appointmentDate,
+        'YYYY-M-d',
+      )}`,
+    );
 
     return appointment;
   }
